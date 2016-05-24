edc148d9ebdd5d420277065e15dadda229131bec
abo <zubo.yu9@gmail.com>
2016-05-24 02:32:43 +0000

    dynamic programming for regular expression matching

---
+++ pattermatch2.cpp
@@ -0,0 +1,146 @@
+#include <string.h>
+#include <iostream>
+using namespace std;
+
+bool isMatch(const char *s, const char *p)
+{
+    int slen = strlen(s);
+    int plen = strlen(p);
+    bool *match = new bool[slen + 1];
+
+    for (int i = 0; i < slen + 1; i++)
+        match[i] = false;
+
+    match[slen] = true;
+    for (int j = plen - 1; j >= 0; j--)
+    {
+        if (p[j] == '*')
+        {
+            for (int i = slen -1; i >= 0; i--)
+            {
+                match[i] = match[i] || match[i+1] && (p[j-1] == '.' || p[j-1] == s[i]);
+            }
+            j--;
+        }
+        else
+        {
+            for (int i = 0; i < slen; i++)
+            {
+                match[i] = match[i+1] && (p[j] == '.' || p[j] == s[i]);
+            }
+            match[slen] = false;
+
+        }
+    }
+
+    bool matched = match[0];
+    delete match;
+    match = NULL;
+
+    return matched;
+}
+	
+void test_match(const char *s, const char *p)
+{
+    std::cout << "source : " << s << std::endl;
+    std::cout << "pattern: " << p << std::endl;
+    bool match = isMatch(s, p);
+    if (match)
+        std::cout << "result : matched" << std::endl;
+    else
+        std::cout << "result : not matched" << std::endl;
+}
+
+void test1()
+{
+    const char *s = "ab";
+    const char *p = "a*b";
+    test_match(s, p);
+}
+
+void test2()
+{
+    const char *s = "ab";
+    const char *p = "a*ab";
+    test_match(s, p);
+}
+
+void test3()
+{
+    const char *s = "ab";
+    const char *p = "a*a*ab";
+    test_match(s, p);
+}
+
+void test4()
+{
+    const char *s = "ab";
+    const char *p = "a*a*b";
+    test_match(s, p);
+}
+
+void test5()
+{
+    const char *s = "ab";
+    const char *p = "a*aa*b";
+    test_match(s, p);
+}
+
+void test6()
+{
+    const char *s = "ab";
+    const char *p = "b*a*b";
+    test_match(s, p);
+}
+
+void test7()
+{
+    const char *s = "ab";
+    const char *p = "b*aa*b";
+    test_match(s, p);
+}
+
+void test8()
+{
+    const char *s = "ab";
+    const char *p = "b*a*b";
+    test_match(s, p);
+}
+
+void test9()
+{
+    const char *s = "ab";
+    const char *p = "b*a*ab";
+    test_match(s, p);
+}
+
+void test10()
+{
+    const char *s = "ab";
+    const char *p = "ab*a*ab";
+    test_match(s, p);
+}
+
+int main(int argc, char** argv)
+{
+    test1();
+    std::cout << std::endl;
+    test2();
+    std::cout << std::endl;
+    test3();
+    std::cout << std::endl;
+    test4();
+    std::cout << std::endl;
+    test5();
+    std::cout << std::endl;
+    test6();
+    std::cout << std::endl;
+    test7();
+    std::cout << std::endl;
+    test8();
+    std::cout << std::endl;
+    test9();
+    std::cout << std::endl;
+    test10();
+    return 0;
+}
